af232a945985ce93ac554199c2dc60e8357c3059
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2024-04-23 22:41:31 +0000

    refactor: url definitions

--- mnotify-sms-javaclient/src/main/java/com/mnotify/constants/URLDefinitions.java
+++ mnotify-sms-javaclient/src/main/java/com/mnotify/constants/URLDefinitions.java
@@ -12,5 +12,13 @@ public class URLDefinitions {
     public static String CONTACT_ENDPOINT = BASE_API_ENDPOINT + "/contact";
     public static String GROUP_ENDPOINT = BASE_API_ENDPOINT + "/group";
     public static String MESSAGE_ENDPOINT = BASE_API_ENDPOINT + "/message";
+    public static String SMS_BALANCE = BASE_API_ENDPOINT + "/balance/sms";
+    public static String SMS_DELIVERY_REPORT = BASE_API_ENDPOINT + "/campaign";
+    public static String VOICE_BALANCE = BASE_API_ENDPOINT + "/balance/voice";
+    public static String SPECIFIC_SMS_DELIVERY_REPORT = BASE_API_ENDPOINT + "/status";
+    public static String PERIODIC_DELIVERY_REPORT = BASE_API_ENDPOINT + "/report";
+    public static String VOICE_CALL_REPORT = BASE_API_ENDPOINT + "/calls";
+    public static String SPECIFIC_VOICE_CALL_REPORT = BASE_API_ENDPOINT + "/call-status";
+    public static String PERIODIC_VOICE_CALL_REPORT = BASE_API_ENDPOINT + "/call-period";
 
 }
