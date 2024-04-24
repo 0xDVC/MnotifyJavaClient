036d446df288625487599b651d036d94b9605a45
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2024-04-24 00:07:56 +0000

    refactor: requestbuilder class

--- mnotify-sms-javaclient/src/main/java/com/mnotify/common/RequestBuilder.java
+++ mnotify-sms-javaclient/src/main/java/com/mnotify/common/RequestBuilder.java
@@ -13,7 +13,7 @@ public class RequestBuilder {
 
     public static  Request buildRequest(String url, int id, String apiKey) {
         return new Request.Builder()
-                .url(url + id)
+                .url(url + "/" + id)
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Authorization", apiKey)
                 .build();
@@ -21,7 +21,7 @@ public class RequestBuilder {
 
     public static  Request buildRequest(String url, int id1, int id2, String apiKey) {
         return new Request.Builder()
-                .url(url + id1 + id2)
+                .url(url + "/" + id1 + "/" + id2)
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Authorization", apiKey)
                 .build();
