0ee4e0593dd5a82be3c3f282117d98d3a0133265
N E I L  O H E N E <54632119+dvc-77@users.noreply.github.com>
2024-04-25 22:17:24 +0000

    feat: added voice and sms balace methods

--- mnotify-sms-javaclient/src/main/java/com/mnotify/ReportsAndStats.java
+++ mnotify-sms-javaclient/src/main/java/com/mnotify/ReportsAndStats.java
@@ -1,10 +1,36 @@
 package com.mnotify;
 
 
-public class ReportsAndStats {
-    private final String apiKey;
+import com.mnotify.common.RequestBuilder;
+import com.mnotify.common.RequestExecutor;
+import com.mnotify.constants.URLDefinitions;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
-    public ReportsAndStats(String apiKey) {
-        this.apiKey = apiKey;
+import java.io.IOException;
+
+public class ReportsAndStats {
+    RequestBuilder builder;
+    public ReportsAndStats(String API_KEY) {
+        builder = new RequestBuilder(API_KEY);
     }
+
+    public ResponseBody checkSMSBalance() throws IOException {
+        builder.URL = URLDefinitions.SMS_BALANCE;
+        Request request = builder.buildRequest();
+
+        return RequestExecutor.executeRequest(request);
+    }
+
+    public ResponseBody checkVoiceBalance() throws IOException {
+        builder.URL = URLDefinitions.VOICE_BALANCE;
+        Request request = builder.buildRequest();
+
+        return RequestExecutor.executeRequest(request)
+    }
+
+//    public ResponseBody smsDeliveryReport() {
+//        builder.URL = URLDefinitions.SMS_DELIVERY_REPORT;
+//        Request request  = builder.buildRequest();
+//    }
 }
